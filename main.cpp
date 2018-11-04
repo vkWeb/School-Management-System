ba246dfbb97472ead1985bce9504bfb7706d0553
Shruti <agrawalshruti577@gmail.com>
2018-11-04 15:08:23 +0530

    removed unnecessary code

--- main.cpp
+++ main.cpp
@@ -1,14 +1,3 @@
 /*** 
 This is just the boilerplate(sample/template) code. The real adventure will begin soon *_*!
 */
-
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-  cout << "Hello World.";
-  cout << "Lets welcome bossi.";
-  return 0;
-}
