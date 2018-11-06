dd57aac7bb4c4fb3d78a406fbbc13609d178172d
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2018-11-06 16:47:14 +0530

    fix(app): Fixes intelliSense and compiling error
    The compiler path is changed to fix intelliSense and to allow smooth
    compilation of program.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 /*** 
 This is just the boilerplate(sample/template) code. The real adventure will begin soon :P!
 */
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-  cout<<"hello world";
+  cout << "hello world";
   return 0;
 }
