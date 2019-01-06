3378049ab2108ad6a13ef5b8b08fc9a2bedcdcf0
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2019-01-06 15:19:40 +0530

    :boom: feat: Update data! :boom: (#31)
    - Student, teacher and staff data records can be updated.
    - Simplified teacher ID generation logic.

--- utils/utilities.h
+++ utils/utilities.h
@@ -1,5 +1,4 @@
 // This file contains functions required for proper I/O validation
-// and often called functions
 #include <iostream>
 using namespace std;
 
@@ -25,35 +24,6 @@ void validateCharInput(char &userInput, char expectedInput[], short expectedInpu
       cin >> userInput;
     }
   } while (flag == 0);
-}
-
-// Verifies teacher subject code, checks if it is between 1 to 5 or not
-void teacherSubjectCodeVerifier(short teacherSubjectCode[], short inputAtIndex)
-{
-  while (teacherSubjectCode[inputAtIndex] < 1 || teacherSubjectCode[inputAtIndex] > 5)
-  {
-    cout << "Sorry, we received a wrong subject code. Please enter subject code between 1 to 5: ";
-    cin >> teacherSubjectCode[inputAtIndex];
-  }
-}
-
-// Sets teacher's subject by using subject code
-// Subject codes and the respective subjects: 1 = Science, 2 = Maths, 3 = English, 4 = Hindi, 5 = Social Studies
-void setTeacherSubjects(short teacherSubjectCode[], char teacherSubjects[])
-{
-  for (int i = 0; i < 3; ++i)
-  {
-    if (teacherSubjectCode[i] == 1)
-      strcat(teacherSubjects, "Science ");
-    else if (teacherSubjectCode[i] == 2)
-      strcat(teacherSubjects, "Maths ");
-    else if (teacherSubjectCode[i] == 3)
-      strcat(teacherSubjects, "English ");
-    else if (teacherSubjectCode[i] == 4)
-      strcat(teacherSubjects, "Hindi ");
-    else if (teacherSubjectCode[i] == 5)
-      strcat(teacherSubjects, "Social Studies ");
-  }
 }
 
 // Validate marks
